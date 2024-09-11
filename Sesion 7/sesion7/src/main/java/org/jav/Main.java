a643d869794dd34a8ac6dd6a22c8f6c711d14719
gabrielgallegos18 <gagallegos@uamv.edu.ni>
2024-09-10 18:44:38 -0600

    agregue un arreglo

--- Sesion 7/sesion7/src/main/java/org/jav/Main.java
+++ Sesion 7/sesion7/src/main/java/org/jav/Main.java
@@ -1,17 +1,16 @@
 package org.jav;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        int [] ages = new int[10];
+        ages[0] = 15;
+        ages[1] = 25;
+        ages[2] = 30;
+        ages[3] = 36;
+        ages[4] = 33;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        for (int i = 0; i < 5; i++) {
+            System.out.println(ages[i]);
         }
     }
 }
